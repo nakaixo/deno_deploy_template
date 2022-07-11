db748c2cc71b087553eab855cb8a7c166594a88a
McKayla Washburn <mckayla@hey.com>
2022-07-10 20:24:04 -0600

    upgrade std

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.147.0/http/server.ts";
 import { main } from "./build/dev/javascript/deno_deploy_template/dist/deno_deploy_template.mjs";
 
 function handler(req) {
